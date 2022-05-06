22dff0eaae549231afea0fdf653b676edd5f39b9
Black - BKP <black.bkp@gmail.com>
2022-05-06 17:27:53 +0700

    Add project files.

---
+++ PrintPdf/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrintPdf.Models;
+using Rotativa;
+using Rotativa.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrintPdf.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Report()
+        {
+            return View();
+        }
+
+        public IActionResult PrintReport()
+        {
+            return new ViewAsPdf("Report");
+        }
+
+        public IActionResult DailyReport()
+        {
+            return View();
+        }
+
+        public IActionResult PrintDailyReport()
+        {
+            return new ViewAsPdf("DailyReport");
+        }
+
+        public IActionResult OvertimeRequest()
+        {
+            return View();
+        }
+
+        public IActionResult PrintOvertimeRequest()
+        {
+            return new ViewAsPdf("OvertimeRequest");
+        }
+    }
+}
