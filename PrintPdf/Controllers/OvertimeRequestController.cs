c08655fde6d2b725270c3c756a4d46648deb4264
Black - BKP <black.bkp@gmail.com>
2022-05-09 15:22:44 +0700

    gg

--- PrintPdf/Controllers/OvertimeRequestController.cs
+++ PrintPdf/Controllers/OvertimeRequestController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PrintPdf.Models;
+using Rotativa.AspNetCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,70 @@ namespace PrintPdf.Controllers
 {
     public class OvertimeRequestController : Controller
     {
+        static List<FormOvertimeRequestModel> requests;
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public List<FormOvertimeRequestModel> GetRequests()
+        {
+            requests = new List<FormOvertimeRequestModel>();
+            FormOvertimeRequestModel req1 = new FormOvertimeRequestModel()
+            {
+                form_id = "2022APR0001",
+                employee_name = "Korakod Phongdee",
+                job_id = "J999999",
+                location = "HQ",
+                site_name = null,
+                withdrawal = true,
+                normal_start_date = new DateTime(2022, 4, 1),
+                normal_stop_date = new DateTime(2022,4,30),
+                normal_start_time = new TimeSpan(17,30,0),
+                normal_stop_time = new TimeSpan(22,0,0),
+                offday_start_date = new DateTime(2022, 4, 1),
+                offday_stop_date = new DateTime(2022, 4, 30),
+                offday_start_time = new TimeSpan(8, 30, 0),
+                offday_stop_time = new TimeSpan(17, 30, 0),
+                objective = "TEST PRINT REPORT"
+            };
+            requests.Add(req1);
+
+            FormOvertimeRequestModel req2 = new FormOvertimeRequestModel()
+            {
+                form_id = "2022APR0002",
+                employee_name = "Korakod Phongdee",
+                job_id = "J000001",
+                location = "Site",
+                site_name = "Ladkrabang",
+                withdrawal = false,
+                normal_start_date = new DateTime(2022, 4, 1),
+                normal_stop_date = new DateTime(2022, 4, 30),
+                normal_start_time = new TimeSpan(17, 30, 0),
+                normal_stop_time = new TimeSpan(22, 0, 0),
+                offday_start_date = new DateTime(2022, 4, 1),
+                offday_stop_date = new DateTime(2022, 4, 30),
+                offday_start_time = new TimeSpan(8, 30, 0),
+                offday_stop_time = new TimeSpan(17, 30, 0),
+                objective = "TEST PRINT REPORT 2"
+            };
+            requests.Add(req2);
+            return requests;
+        }
+
+        public IActionResult RequestForm(string req_id)
+        {
+            FormOvertimeRequestModel request = requests.Where(w => w.form_id == req_id).FirstOrDefault();
+            return View(request);
+        }
+
+        [HttpGet]
+        public IActionResult PrintRequestForm(string req_id)
+        {
+            RequestForm(req_id);
+            return new ViewAsPdf("RequestForm", req_id);
         }
     }
 }
---
+++ PrintPdf/Models/FormOvertimeRequestModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrintPdf.Models
+{
+    public class FormOvertimeRequestModel
+    {
+        public string form_id { get; set; }
+        public string employee_name { get; set; }
+        public string job_id { get; set; }
+        public string location { get; set; }
+        public string site_name { get; set; }
+        public bool withdrawal { get; set; }
+        public DateTime normal_start_date { get; set; }
+        public DateTime normal_stop_date { get; set; }
+        public TimeSpan normal_start_time { get; set; }
+        public TimeSpan normal_stop_time { get; set; }
+        public DateTime offday_start_date { get; set; }
+        public DateTime offday_stop_date { get; set; }
+        public TimeSpan offday_start_time { get; set; }
+        public TimeSpan offday_stop_time { get; set; }
+        public string objective { get; set; }
+    }
+}
