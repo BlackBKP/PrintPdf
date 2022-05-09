6f5db97a6c9e824c684469facc39af536ec75c2b
Black - BKP <black.bkp@gmail.com>
2022-05-09 11:19:39 +0700

    GG

---
+++ PrintPdf/Controllers/DailyReportController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrintPdf.Controllers
+{
+    public class DailyReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
